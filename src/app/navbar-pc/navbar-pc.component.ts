8b460e71fbbca070f403a523c05580e60c4a7167
RamonLS1509 <ramonlopezsalmeron@gmail.com>
2025-05-16 16:54:48 +0200

    commit 3

--- src/app/navbar-pc/navbar-pc.component.ts
+++ src/app/navbar-pc/navbar-pc.component.ts
@@ -1,11 +1,18 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, HostListener, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-pc',
   imports: [],
-  templateUrl: 'navbar-pc.component.html'
+  templateUrl: 'navbar-pc.component.html',
 })
 export class NavbarPcComponent {
 
+  mouseX = 0;
+  mouseY = 0;
+
+  onMouseMove(event: MouseEvent) {
+    this.mouseX = event.offsetX;
+    this.mouseY = event.offsetY;
+  }
 
 }
